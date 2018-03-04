374023f67a9bddc79aff43604c60a674b65fcf33
duemaster <zhangbsn@gmail.com>
2018-03-04 21:22:26 +0800

    Added Camera Direction jump

--- test.js
+++ test.js
@@ -1,12 +1,16 @@
 $('body').keyup((e) => {
     if (e.keyCode == 32) {
-        var box = document.getElementById("box");
+        // var box = document.getElementById("box");
+        // console.log(box.getAttribute("position"));
 
-        console.log(box.getAttribute("position"));
+        //Get camera facing location
+        let camera_direction = document.getElementById("camera").object3D.getWorldDirection();
+
+        let forceAmount = -40;
 
         box.body.applyImpulse(
             /* impulse */
-            new CANNON.Vec3(0, 20, 0),
+            new CANNON.Vec3(forceAmount * camera_direction.x, forceAmount * camera_direction.y, forceAmount * camera_direction.z),
             /* world position */
             // new CANNON.Vec3().copy(box.getAttribute("position"))
             new CANNON.Vec3().copy({ x: 0, y: 0, z: 0 })
