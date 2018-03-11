0beefc417443cf39412cf5f8870de4e02cfb6842
duemaster <zhangbsn@gmail.com>
2018-03-12 01:42:46 +0800

    Update Gravity

--- gravity-component.js
+++ gravity-component.js
@@ -1,14 +1,21 @@
 AFRAME.registerComponent('gravity', {
-    schema: {},
+    schema: {
+        mass: { type: 'int', default: 5 },
+        gravity_range: { type: 'int', default: 1000 }
+    },
     init: function() {
         console.log("Gravity Component");
     },
     update: function() {},
     tick: function() {
+
+        let asteriod = this.el;
+        let player = document.getElementById("box");
+
         //get coordinate of asteriod
-        let asteriodCoord = this.el.object3D.getWorldPosition();
+        let asteriodCoord = asteriod.object3D.getWorldPosition();
         //get coordinate of player
-        let playerCoord = document.getElementById("box").object3D.getWorldPosition();
+        let playerCoord = player.object3D.getWorldPosition();
 
         //Get vector from player to asteriod
         let gravVec = new THREE.Vector3();
@@ -18,13 +25,14 @@ AFRAME.registerComponent('gravity', {
         let distance = asteriodCoord.distanceTo(playerCoord);
 
         //Get apply force on player
-        let player = document.getElementById("box");
-        let gravForce = 5 / Math.pow(distance, 2);
+        let gravForce = 5 * asteriod.mass / Math.pow(distance, 2);
 
-        // console.log(playerCoord);
-        // console.log(asteriodCoord);
-        // console.log(gravVec);
         try {
+            //Apply no force if outside gravity range
+            if (distance > asteriod.gravity_range) {
+                return;
+            }
+
             player.body.applyImpulse(
                 /* impulse */
                 new CANNON.Vec3(gravForce * gravVec.x, gravForce * gravVec.y, gravForce * gravVec.z),
