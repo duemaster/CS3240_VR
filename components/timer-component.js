f8cb41b81952185e268d9273b1ae82cb8a59da98
duemaster <zhangbsn@gmail.com>
2018-03-23 17:15:11 +0800

    Added Timer

---
+++ components/timer-component.js
@@ -0,0 +1,18 @@
+AFRAME.registerComponent('timer', {
+    schema: {
+        currentTime: {type: 'number', default: 0}
+    },
+    init: function() {
+        console.log("Timer Component");
+    },
+    update: function() {},
+    tick: function() {
+        this.data.currentTime += 1;
+        let currentSecondElapsed = Math.floor(this.data.currentTime/ 60);
+        let momentTimer = moment().hour(0).minute(0).second(currentSecondElapsed);
+        this.el.setAttribute("value", momentTimer.format("mm:ss"));
+    },
+    remove: function() {},
+    pause: function() {},
+    play: function() {}
+});
