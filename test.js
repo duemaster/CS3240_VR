d27a322f04e0b7f989cb12fb509e6f4f8e9c45fb
duemaster <zhangbsn@gmail.com>
2018-03-28 13:50:20 +0800

    added enter vr mode auto

--- test.js
+++ test.js
@@ -1,3 +1,15 @@
+// switch to stereoscopic mode directly on page load, this needs to be after the a-scene loads.
+var scene = document.querySelector('a-scene');
+if (scene.hasLoaded) {
+    scene.enterVR();
+} else {
+    scene.addEventListener('loaded', function () {
+        setTimeout(function () {
+            scene.enterVR();
+        }, 1000);
+    });
+}
+
 var jumpInDirection = function() {
     let player = document.getElementById("box");
 
